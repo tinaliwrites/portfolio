09242c8f671ed19b159b8d7228118ce81abc9ea9
minioncode <tinany721@gmail.com>
2026-03-25 13:50:11 -0400

    Clean up unused pages and components

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Writing from "./pages/Writing";
-import Books from "./pages/Books";
-
 import MoreAbout from "./pages/MoreAbout";
 import ResumePage from "./pages/ResumePage";
 
@@ -22,9 +20,7 @@ const App = () => (
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<Home />} />
           <Route path="/writing" element={<Writing />} />
-          <Route path="/books" element={<Books />} />
           <Route path="/more-about" element={<MoreAbout />} />
-
           <Route path="/resume" element={<ResumePage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<Navigate to="/about" replace />} />
