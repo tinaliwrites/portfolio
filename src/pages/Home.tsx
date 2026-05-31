f62061c877bb8b2ad9d3938534d0bf20b2c5cccf
minioncode <tinany721@gmail.com>
2026-05-30 23:47:11 -0400

    Change final year to senior year at Yale in bio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,7 +17,7 @@ const Home = () => {
               I previously served as managing editor of <em>The New Journal</em>, reported for <em>The Sacramento Bee</em> and the <em>Yale Daily News</em>, and freelanced elsewhere.
             </p>
             <p>
-              In my final year, I'll be a Yale Journalism Initiative coordinating fellow, literary editor at the <em>Yale Literary Magazine</em>, and managing editor for the <em>Elm City Echo</em>. I study English literature and will write a poetry thesis.
+              In my senior year at Yale, I'll be a Yale Journalism Initiative coordinating fellow, literary editor at the <em>Yale Literary Magazine</em>, and managing editor for the <em>Elm City Echo</em>. I study English literature and will write a poetry thesis.
             </p>
             <p>
               You can email me at <a href="mailto:tina.jh.li@yale.edu" className="underline decoration-primary/30 hover:decoration-primary transition-colors underline-offset-4">tina.jh.li@yale.edu</a>, and follow my <a href="https://www.linkedin.com/in/tinaliyale/" target="_blank" rel="noopener noreferrer" className="underline decoration-primary/30 hover:decoration-primary transition-colors underline-offset-4">LinkedIn</a> and <a href="https://x.com/its_tinali" target="_blank" rel="noopener noreferrer" className="underline decoration-primary/30 hover:decoration-primary transition-colors underline-offset-4">X</a>. While you’re here, you can read my clips and learn even more <Link to="/more-about" className="underline decoration-primary/30 hover:decoration-primary transition-colors underline-offset-4">about me.</Link>
